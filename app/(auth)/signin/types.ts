a9309ac54a3c4e7d9bda3c64ce13b0f5a1f78c54
ibrvheem <i.aliyu019@gmail.com>
2026-08-23 01:22:18 +0100

    Refactor code structure for improved readability and maintainability

--- app/(auth)/signin/types.ts
+++
@@ -1,9 +0,0 @@
-import z from "zod";
-
-export const signinPayload = z.object({
-    email: z.string().email(),
-    password: z.string()
-
-})
-
-export type SignInPayload = z.infer<typeof signinPayload>
